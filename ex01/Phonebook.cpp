b0c025c650902122af5fd21cbd712d02404c8766
Jorge Bortolotti Suarez <jbortolo@car15s4.42barcelona.com>
2024-01-26 17:14:16 +0100

    mas o menos terminada la logica del add y el exit

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -1,9 +1,41 @@
+#include "PhoneBook.hpp"
 #include "program.h"
 
-
-void add_contact(PhoneBook *book)
+PhoneBook::PhoneBook()
 {
-	string u_input = "";
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+
+void PhoneBook::recount_ids()
+{
+	for(int i = 0; i < 7; i++)
+		contacts[i].set_id(i + 1);
+}
+
+void PhoneBook::rotate_list()
+{
+	Contact tmp[8];
+	int j = 0;
+
+	tmp[7] = contacts[0];
+	for (int i = 1 ; i < 6; i++)
+	{
+		tmp[j] = contacts[i];
+		j++;
+	}
+	for (int i = 0; i < 7; i++)
+		contacts[i] = tmp[i];
+	recount_ids();
+}
+
+void PhoneBook::add_contact(void)
+{
+	static int id = 0;
+	std::string u_input[5];
+	u_input[0] = "";
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -11,34 +43,55 @@ void add_contact(PhoneBook *book)
 		{
 			case  0 :
 			{
-				cout << "enter contact first name" << endl;
-				getline(cin, u_input);
+				std::cout << "enter contact first name" << std::endl;
+				std::getline(std::cin, u_input[i]);
 				break;
 			}
 			case  1 :
 			{
-				cout << "enter contact last name" << endl;
-				getline(cin, u_input);
+				std::cout << "enter contact last name" << std::endl;
+				std::getline(std::cin, u_input[i]);
 				break;
 			}
 			case  2 :
 			{
-				cout << "enter contact nickname" << endl;
-				getline(cin, u_input);
+				std::cout << "enter contact nickname" << std::endl;
+				std::getline(std::cin, u_input[i]);
 				break;
 			}
 			case   3:
 			{
-				cout << "enter contact phone number" << endl;
-				getline(cin, u_input);
+				std::cout << "enter contact phone number" << std::endl;
+				std::getline(std::cin, u_input[i]);
 				break;
 			}
 			case   4:
 			{
-				cout << "enter contact darkest secret" << endl;
-				getline(cin, u_input);
+				std::cout << "enter contact darkest secret" << std::endl;
+				std::getline(std::cin, u_input[i]);
 				break;
 			}
+			default :
+				break;
 		}
+		if (u_input[i].empty() || std::cin.fail())
+		{
+			std::cout << "no input field can be empty" << std::endl;
+			i--;
+		}
+		if (i == 3 && !all_of(u_input[i].begin(), u_input[i].end(), ::isdigit))
+		{
+			std::cout << "arguments must be numeric" << std::endl;
+			i--;
+		}
+	}
+	if (id <= 7)
+	{
+		contacts[id] = Contact(u_input[0], u_input[1], u_input[2], u_input[3], u_input[4], id+1);
+	}
+	if (id > 7)
+	{
+		contacts[0] = Contact(u_input[0], u_input[1], u_input[2], u_input[3], u_input[4], 1);
+		rotate_list();
 	}
 }
